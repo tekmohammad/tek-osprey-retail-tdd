96e87614e0e1564f1e8249e14bb943521a7533f2
mohammad <mohammad@tekschool.us>
2024-03-19 21:03:53 -0400

    TDD Day 2

--- src/test/java/tek/tdd/pages/LoginPage.java
+++ src/test/java/tek/tdd/pages/LoginPage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import tek.tdd.utilities.SeleniumUtilities;
 
+import java.util.List;
+
 public class LoginPage extends SeleniumUtilities {
 
     public LoginPage(){
@@ -21,4 +23,19 @@ public class LoginPage extends SeleniumUtilities {
 
     @FindBy(id = "loginBtn")
     public WebElement loginButton;
+
+    @FindBy(xpath = "//div[@class='error']")
+    public WebElement errorMessage;
+
+    //If the Locator return more than 1 Elements
+    @FindBy(tagName = "a")
+    public List<WebElement> anchorElements;
+
+
+    public void doLogin(String username, String password) {
+        sendText(emailInput, username);
+        sendText(passwordInput, password);
+        clickOnElement(loginButton);
+    }
+
 }
--- src/test/java/tek/tdd/test/regression/SecurityTest.java
+++ src/test/java/tek/tdd/test/regression/SecurityTest.java
@@ -1,6 +1,7 @@
 package tek.tdd.test.regression;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import tek.tdd.base.BaseUITests;
 import tek.tdd.pages.HomePage;
@@ -8,9 +9,17 @@ import tek.tdd.pages.LoginPage;
 
 public class SecurityTest extends BaseUITests {
 
+    @DataProvider(name = "validCredentials")
+    public String[][] validCredentialProvider() {
+        String[][] data = {
+                {"mohammad_osprey@tekschool.us", "12345678Ab@"},
+                {"Mansoor@gmail.com" , "Ruya@2014"}
+        };
+        return data;
+    }
 
-    @Test
-    public void validLoginWithValidCredentials() {
+    @Test(dataProvider = "validCredentials")
+    public void validLoginWithValidCredentials(String username, String password) {
         /*
         Navigate to sign in page and sing in with valid username and password.
         Validate user successfully signed in.
@@ -18,17 +27,45 @@ public class SecurityTest extends BaseUITests {
         HomePage homePage = new HomePage();
         clickOnElement(homePage.signInButton);
 
-
         LoginPage loginPage = new LoginPage();
 
-        sendText(loginPage.emailInput, "mohammad_osprey@tekschool.us");
-        sendText(loginPage.passwordInput, "12345678Ab@");
-        clickOnElement(loginPage.loginButton);
+        loginPage.doLogin(username, password);
 
         boolean isLogoutBtnDisplayed = isElementDisplayed(homePage.logoutButton);
 
         Assert.assertTrue(isLogoutBtnDisplayed,
                 "After Success login, Logout button should display in home page");
+    }
+
+
+    @Test(dataProvider = "negativeTestData")
+    public void validateLoginWithInvalidCredentials(
+            String username,
+            String password,
+            String expectedErrorMessage
+    ) {
+        HomePage homePage = new HomePage();
+        clickOnElement(homePage.signInButton);
+
+        LoginPage loginPage = new LoginPage();
+
+        loginPage.doLogin(username, password);
+
+        String actualErrorMessage = getElementText(loginPage.errorMessage);
+        Assert.assertEquals(actualErrorMessage, expectedErrorMessage,
+                "Error Message Should Display");
+    }
+
+
+    @DataProvider(name = "negativeTestData")
+    private String[][] negativeTestData(){
+        String[][] data = {
+                {"wrong@email.com", "12345678Ab@" , "wrong username or password"},
+                {"mohammad_osprey@tekschool.us", "invalid@Password", "wrong username or password"},
+                {"wrong@email.com", "invlaid@Password", "wrong username or password"}
+        };
+
+        return data;
     }
 
 }
