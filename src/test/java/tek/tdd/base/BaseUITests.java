bb9f7cc26bcba4181490e947c114cdf8b6296129
mohammad <mohammad@tekschool.us>
2024-03-20 21:16:47 -0400

    TDD Day 3

--- src/test/java/tek/tdd/base/BaseUITests.java
+++ src/test/java/tek/tdd/base/BaseUITests.java
@@ -1,12 +1,15 @@
 package tek.tdd.base;
 
+import com.aventstack.extentreports.testng.listener.ExtentITestListenerAdapter;
 import org.openqa.selenium.bidi.log.Log;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import tek.tdd.pages.HomePage;
 import tek.tdd.pages.LoginPage;
 import tek.tdd.utilities.SeleniumUtilities;
 
+@Listeners({ExtentITestListenerAdapter.class})
 public class BaseUITests extends SeleniumUtilities {
 
     public HomePage homePage;
